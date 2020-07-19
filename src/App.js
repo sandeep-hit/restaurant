88fb35068d6c8b68f178302bc277fa645e48427a
Sandeep Chaudhary <sandeep.chaudhary.cse21@heritageit.edu.in>
2020-07-19 10:52:21 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-import "./App.css";
+import React, { Component } from 'react';
 import Main from "./components/MainComponent";
-import { DISHES } from "./shared/dishes";
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 
-import { Provider } from "react-redux";
-import { ConfigureStore } from "./redux/configureStore";
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
 const store = ConfigureStore();
 
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
 
   render() {
     return (
-      <div className="App">
-        <Provider store={store}>
-          <BrowserRouter>
-            <div className="App">
-              <Main />
-            </div>
-          </BrowserRouter>
-        </Provider>
-      </div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
